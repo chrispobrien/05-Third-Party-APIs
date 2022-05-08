1e5e160456b63b5aa9ef950f492a6522ae3cdcc6
chrispobrien <chris.obrien57@gmail.com>
2022-05-08 12:59:52 -0400

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # 05-Third-Party-APIs
+
+## Description
+Week 5 of Columbia Engineering Coding Bootcamp tasks us with creating "...a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery."
+
+For a demonstration please browse to my Github pages:
+[https://chrispobrien.github.io/05-Third-Party-APIs/]
+
+Objectives include:
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+* For this exercise, I would have used the blur to save changed event text, but there is a save button, so I use that.
+* A side-effect of using the save button, the user can edit more than one event at the same time, and save them in any order
+* I chose to use [Moment.js](https://momentjs.com/) library even though it is "legacy."
+* I couldn't find an iconic icon matching the demo exactly, I downloaded a public domain svg image instead
+
+## Installation
+
+Using git, issue the command:
+
+```sh
+git clone https://github.com/chrispobrien/05-Third-Party-APIs.git
+```
+This creates the folder 05-Third-Party-APIs within which you will find the project files.  The images folder contains the animated gif supplied by the Bootcamp to demonstrate the intended function of this project.
+
+[![Schedule Demonstration][demo]](./assets/images/05-third-party-apis-homework-demo.gif)
+
+## Usage
+
+Since this is a classroom exercise, I would recommend simply opening the index.html file in a browser on your local machine.
+
+[![Third Party APIs][screenshot]](./assets/images/05-third-party-apis.png)
+
+## Credits
+
+Columbia Coding Bootcamp/Trilogy supplied starter HTML and CSS.  All edits and JS are my own solution to this problem.
+
+## License
+
+© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[demo]: ./assets/images/05-third-party-apis-homework-demo.gif
+[screenshot]: ./assets/images/05-third-party-apis.png
+
---
+++ assets/js/script.js
@@ -0,0 +1,117 @@
+// Load references to commonly used elements
+var currentDayEl = $("#currentDay");
+var mainEl = $("main");
+
+// The schedule array declaration
+// Note array index is 0-8 to represent 0=9 AM, 1=10 AM, etc. to 8=5 PM
+var schedule;
+
+// Load today's date in desired format e.g. Saturday, May 7th
+var now = moment().format("dddd, MMMM Do");
+// Show the current day
+$(currentDayEl).text(now);
+
+// Load schedule from localStorage, if it does not exist create an empty one
+var loadSchedule = function() {
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+    // If no schedule in localStorage
+    if (!schedule) {
+      // Initialize empty schedule array
+      schedule = [];
+      // push empty string
+      for (i=0;i<9;i++) schedule.push("");
+      // Save the new schedule to localStorage
+      saveSchedule();
+    };
+};
+
+// Save the schedule to localStorage
+var saveSchedule = function() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+};
+
+// This loads the schedule, whether empty or full, to the main page
+var loadScheduleEl = function() {
+    // First load the schedule from localStorage
+    loadSchedule();
+
+    // Empty the section of the page with the schedule
+    mainEl.empty();
+
+    // Recreate the schedule
+    for (i=0;i<9;i++) {
+        // time to display is just the hour and AM/PM
+        let time = moment().hour(i+9).format("hA");
+        let hour = $("<div>").addClass("col-2 bg-white p-4 hour").text(time);
+        let p = $("<p>").text(schedule[i]);
+        let text = $("<div>").addClass("col-8 p-2 past").append(p);
+        let icon = $("<img>").addClass("btnInvert").attr("src","./assets/images/floppy-31.svg").attr("alt","Save").attr("width","20px");
+        let save = $("<div>").addClass("col-2 text-center p-4 saveBtn").append(icon);
+        let hourRow = $("<div>").addClass("row").attr("id","list-"+i).append(hour, text, save);
+        $(mainEl).append(hourRow);
+    }
+
+    // Change the background colors to indicate past, present, future
+    updateBackgrounds();
+};
+
+// On click of text div, swap in a textarea with the contents of <p> and focus on it
+$("#schedule").on("click",".col-8", function() {
+    $(this).find("p").replaceWith($("<textarea>")
+    .addClass("form-control")
+    .val($(this).text().trim()));
+    $("textarea").trigger("focus");
+});
+
+/* I'd rather do this with blur but there is a save button, so use it!
+$("#schedule").on("blur","textarea", function() {
+    let text = $(this).val().trim();
+    let element = $(this).closest(".row").attr("id").replace("list-","");
+    schedule[element] = text;
+    saveSchedule();
+    let textP = $("<p>").text(text);
+    $(this).replaceWith(textP);
+
+});
+*/
+
+// Save edited text for this hour
+$("#schedule").on("click",".saveBtn", function() {
+    // Get id of row and load into arrayIndex
+    let arrayIndex = $(this).closest(".row").attr("id").replace("list-","");
+    // Find textarea element if it exists on this row
+    let textArea = $(this).closest(".row").find("textarea");
+    // If no textarea on this row ignore the button click
+    if (textArea.length === 0) return 0;
+    // Load changed text
+    let text = textArea.val().trim();
+    // Save changed text into array
+    schedule[arrayIndex] = text;
+    // Save schedule to localStorage
+    saveSchedule();
+    // Create new <p> element and load it with the new text
+    let textP = $("<p>").text(text);
+    // Replace textarea with p
+    textArea.replaceWith(textP);
+});
+
+// Iterate over rows and color based on past, present and future
+var updateBackgrounds = function() {
+    for (i=0;i<9;i++) {
+        // load hour with the hour right now, subtract 9 to get the list- index
+        let hour = moment().hour()-9;
+        // Find the textElement div
+        let textElement = $("#schedule").find("#list-"+i).find(".col-8");
+        // Remove all time classes
+        $(textElement).removeClass("past present future");
+        // Add a time class based on time right now compared to hour of row
+        if (i<hour) $(textElement).addClass("past");
+        if (i===hour) $(textElement).addClass("present");
+        if (i>hour) $(textElement).addClass("future");
+    }
+};
+
+// Load schedule from localStorage, load page, color text boxes
+loadScheduleEl();
+// Every 60 seconds update backgrounds if hour has changed
+setInterval(updateBackgrounds,60000);
